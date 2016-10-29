a47b69b0404e2863e08b5840fb4f48a105f83635
Yulia <juliemeln@gmail.com>
2016-10-29 15:29:31 +0300

    задание №1

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+//задание 1
+    $name = "Юля";
+    $age = 33;
+    echo 'Меня зовут '.$name.'</br>';
+    echo 'Мне '.$age.' года <br>';
+    echo ' “!|\/’”\<br> ';
+
+//задание 2
+    $pics=80;
+    $flomaster=23;
+    $karandash=40;
+    $kraski=$pics-($flomaster+$karandash);
+    echo $kraski;
+//задание 3
+
+
