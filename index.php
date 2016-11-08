8c06a2d4ca1b94951fc49e92e4bd5da03ade1b75
Jul <juliemeln@gmail.com>
2016-11-08 14:30:07 +0400

    исправлено 5 задание

--- index.php
+++ index.php
@@ -55,23 +55,13 @@ echo "<br>" . 'задание 5' . "<br>";
 $day = 7;
 switch ($day) {
     case '1':
-        echo 'Это рабочий   день';
-        break;
     case '2':
-        echo 'Это рабочий день';
-        break;
     case '3':
-        echo 'Это   рабочий   день';
-        break;
     case '4':
-        echo 'Это   рабочий   день';
-        break;
     case '5':
         echo 'Это  рабочий  день';
         break;
     case '6':
-        echo 'Это   выходной   день';
-        break;
     case '7':
         echo 'Это   выходной   день';
         break;
